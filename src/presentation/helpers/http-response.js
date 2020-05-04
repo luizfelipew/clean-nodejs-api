0111594597c1b3b72146c334ab0fb7b7b45cfca9
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-05-03 23:54:41 -0300

    ensure LoginRouter calls AuthUseCase with correct params

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,16 +1,15 @@
 const MissingParamError = require('./missing-param-error')
-module.exports =
-  class HttpResponse {
-    static badRequest (paramName) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError(paramName)
-      }
-    }
-
-    static serverError () {
-      return {
-        statusCode: 500
-      }
+module.exports = class HttpResponse {
+  static badRequest (paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName)
     }
   }
+
+  static serverError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
